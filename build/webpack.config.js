865098ca0c735e6a0198bd3716ca750cfc60d34c
GarenWang <2270099149@qq.com>
2019-04-01 21:53:24 +0800

    搭建typescript开发环境

---
+++ build/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const isProd = process.env.NODE_ENV === 'production'
+
+const plugins = [
+    new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: ['./dist']
+    }),
+    new HtmlWebpackPlugin({
+        template: './src/template/index.html'
+    })
+];
+module.exports = {
+    mode: isProd ? "production" : "development",
+    entry: "./src/index.ts",
+    output: {
+        filename: "main.js",
+    },
+    resolve: {
+        extensions: ['.js', 'jsx', 'ts', 'tsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude:/node_modules/,
+
+            }
+        ]
+    },
+    devtool: isProd ? false : 'inline-source-map',
+    devServer: {
+        contentBase: "./dist",
+        stats: "errors-only",
+        compress: false,
+        host: 'localhost',
+        port: 8888,
+    },
+    plugins: plugins,
+
+}
